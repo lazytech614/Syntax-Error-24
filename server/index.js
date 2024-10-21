dc4d043b5373492d0898def256151dcf025a34c4
lazytech614 <derupanjan2021@gmail.com>
2024-10-21 15:00:30 +0530

    Save local changes before pulling

--- server/index.js
+++ server/index.js
@@ -19,9 +19,6 @@ import FormData from "form-data";
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
 const port = process.env.PORT || 5000;
 
